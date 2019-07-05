7d0c8fc17a52f251cc665060aad203347d484885
Dolimov Khumoyunmirzo| <humodalim@gmail.com>
2019-07-05 19:33:16 +0500

    Routes has been Added as well as part of Login Form

---
+++ src/components/loginForm.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+
+class LoginForm extends Component {
+  state = {
+    account: { username: " ", password: "" }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const username = this.username.current.value;
+    console.log(username);
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
+  render() {
+    const { account } = this.state;
+
+    return (
+      <div>
+        <h1>Login Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username" />
+            Username
+            <input
+              autoFocus
+              value={account.username}
+              onChange={this.handleChange}
+              id="username"
+              name="username"
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password" />
+            Password
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
+              id="password"
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <button className="btn btn-primary ">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
